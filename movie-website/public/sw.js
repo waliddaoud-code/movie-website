af10b58a95b3c8cc57b3dba2a569c454f5de2bc5
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-07-03 00:43:12 +0100

    Update service worker domain and zone ID

--- movie-website/public/sw.js
+++ movie-website/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11231369
+    "domain": "5gvci.com",
+    "zoneId": 11231587
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
