2d72578aa56b59027ac090c5334352a920653002
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-07-03 21:33:13 +0100

    Update Home.jsx

--- movie-website/src/pages/Home.jsx
+++ movie-website/src/pages/Home.jsx
@@ -2,66 +2,28 @@ import { useEffect, useRef } from "react";
 import "../css/Home.css";
 import MovieRow from "../components/MovieRow/MovieRow";
 import Poster from "../components/Poster";
+import { useAdsterraBanner } from "../assets/useAdsterraBanner.js";
 
 function Home({ movies, tvShows }) {
-  const bannerRef = useRef(null);
-
-  useEffect(() => {
-    if (!bannerRef.current) return;
-
-    // Adsterra banner config
-    window.atOptions = {
-      key: "771b0ab5bd5d03c5d394b2174692df1a",
-      format: "iframe",
-      height: 60,
-      width: 468,
-      params: {},
-    };
-
-    const script = document.createElement("script");
-    script.src = "https://www.highperformanceformat.com/771b0ab5bd5d03c5d394b2174692df1a/invoke.js";
-    script.async = true;
-
-    bannerRef.current.appendChild(script);
-
-    return () => {
-      if (bannerRef.current) {
-        bannerRef.current.innerHTML = "";
-      }
-    };
-  }, []);
+  const bannerRef = useAdsterraBanner({
+    key: "771b0ab5bd5d03c5d394b2174692df1a",
+    width: 468,
+    height: 60,
+  });
 
   return (
     <div className="home">
       <Poster movies={movies.trendingAll} />
 
-      <MovieRow
-        title="Popular"
-        movies={movies.popular}
-        series={tvShows.popular}
-      />
+      <MovieRow title="Popular" movies={movies.popular} series={tvShows.popular} />
 
-      {/* Adsterra Banner */}
       <div
         ref={bannerRef}
-         style={{ width: 470, margin: "auto" }}
+        style={{ width: 468, height: 60, margin: "20px auto" }}
       ></div>
 
-      <MovieRow
-        title="Trending"
-        movies={movies.trending}
-        series={tvShows.trending}
-      />
-         <div
-        ref={bannerRef}
-           style={{ width: 470 , margin: "auto" }}
-      ></div>
-
-      <MovieRow
-        title="Top Rated"
-        movies={movies.topRated}
-        series={tvShows.topRated}
-      />
+      <MovieRow title="Trending" movies={movies.trending} series={tvShows.trending} />
+      <MovieRow title="Top Rated" movies={movies.topRated} series={tvShows.topRated} />
     </div>
   );
 }
