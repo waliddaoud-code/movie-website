aba3e936b39329d0328fd2f8792a8640e46ba5c6
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-07-03 19:03:53 +0100

    Implement popunder ad loading in Home component
    Added popunder ad functionality with cooldown logic.

--- movie-website/src/pages/Home.jsx
+++ movie-website/src/pages/Home.jsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
 import "../css/Home.css";
 import MovieRow from "../components/MovieRow/MovieRow";
 import Poster from "../components/Poster";
 
 function Home({ movies, tvShows }) {
+  useEffect(() => {
+  const KEY = "last_popunder";
+  const COOLDOWN = 45 * 1000;
+
+  const loadPopunder = () => {
+    const last = Number(localStorage.getItem(KEY) || "0");
+
+    if (Date.now() - last < COOLDOWN) return;
+
+    const script = document.createElement("script");
+    script.dataset.zone = "11235916";
+    script.src = "https://al5sm.com/tag.min.js";
+
+    document.body.appendChild(script);
+
+    localStorage.setItem(KEY, Date.now().toString());
+  };
+
+  loadPopunder();
+
+  const interval = setInterval(loadPopunder, COOLDOWN);
+
+  return () => {
+    clearInterval(interval);
+  };
+}, []);
   return (
     <div className="home">
       <Poster movies={movies.trendingAll} />
