5db68c3826c916b6789521ace59d6008f86e2087
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-07-03 22:20:57 +0100

    Load external ad script in Home component
    Added a useEffect hook to load an external script for ads.

--- movie-website/src/pages/Home.jsx
+++ movie-website/src/pages/Home.jsx
@@ -4,7 +4,27 @@ import MovieRow from "../components/MovieRow/MovieRow";
 import Poster from "../components/Poster";
 import { useAdsterraBanner } from "../assets/useAdsterraBanner.js";
 
+
+
 function Home({ movies, tvShows }) {
+  useEffect(() => {
+  if (document.getElementById("container-47c07c4e1d2c3eb3abaa13087179e0d3"))
+    return;
+
+  const script = document.createElement("script");
+  script.src =
+    "https://pl30192940.effectivecpmnetwork.com/47c07c4e1d2c3eb3abaa13087179e0d3/invoke.js";
+  script.async = true;
+  script.setAttribute("data-cfasync", "false");
+
+  document.body.appendChild(script);
+
+  return () => {
+    script.remove();
+  };
+}, []);
+  
+  
   const bannerRef = useAdsterraBanner({
     key: "771b0ab5bd5d03c5d394b2174692df1a",
     width: 468,
@@ -23,6 +43,11 @@ function Home({ movies, tvShows }) {
       ></div>
 
       <MovieRow title="Trending" movies={movies.trending} series={tvShows.trending} />
+
+      <div
+  id="container-47c07c4e1d2c3eb3abaa13087179e0d3"
+  style={{ margin: "20px auto" }}
+></div>
 
        
       
