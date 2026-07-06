68cb8fa2c52e2f93731857a4701809efd3c80f7f
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-07-06 16:55:20 +0100

    Replace useAdsterraBanner with AdsterraBanner component

--- movie-website/src/pages/Home.jsx
+++ movie-website/src/pages/Home.jsx
@@ -2,24 +2,14 @@ import { useEffect, useRef } from "react";
 import "../css/Home.css";
 import MovieRow from "../components/MovieRow/MovieRow";
 import Poster from "../components/Poster";
-import { useAdsterraBanner } from "../assets/useAdsterraBanner.js";
+import { AdsterraBanner } from "../assets/useAdsterraBanner.js";
 
 
 
 function Home({ movies, tvShows }) {
  
   
-  
-  const bannerRef = useAdsterraBanner({
-    key: "771b0ab5bd5d03c5d394b2174692df1a",
-    width: 468,
-    height: 60,
-  });
-  const bannerRef2 = useAdsterraBanner({
-    key: "b37c8ffb927736e7563e62d79974015f",
-    width: 320,
-    height: 50,
-  });
+
 
   return (
     <div className="home">
@@ -27,17 +17,20 @@ function Home({ movies, tvShows }) {
 
       <MovieRow title="Popular" movies={movies.popular} series={tvShows.popular} />
 
-      <div
-        ref={bannerRef}
-        style={{ width: 468, height: 60, margin: "20px auto" }}
-      ></div>
+    <AdsterraBanner
+    adKey="771b0ab5bd5d03c5d394b2174692df1a"
+    width={468}
+    height={60}
+/>
+
 
       <MovieRow title="Trending" movies={movies.trending} series={tvShows.trending} />
 
-        <div
-        ref={bannerRef2}
-        style={{ width: 468, height: 60, margin: "20px auto" }}
-      ></div>
+       <AdsterraBanner
+    adKey="b37c8ffb927736e7563e62d79974015f"
+    width={320}
+    height={50}
+/>
        
       
       <MovieRow title="Top Rated" movies={movies.topRated} series={tvShows.topRated} />
