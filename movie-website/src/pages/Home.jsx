599b8a7150d6fd10f3381def3f3244e1f89aef0f
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-07-23 11:05:30 +0100

    Update Home.jsx

--- movie-website/src/pages/Home.jsx
+++ movie-website/src/pages/Home.jsx
@@ -2,8 +2,7 @@ import { useEffect, useRef } from "react";
 import "../css/Home.css";
 import MovieRow from "../components/MovieRow/MovieRow";
 import Poster from "../components/Poster";
-import  AdsterraBanner  from "../assets/useAdsterraBanner";
-import  NativeAd  from "../assets/NativeAd";
+
 
 
 function Home({ movies, tvShows }) {
@@ -18,19 +17,14 @@ function Home({ movies, tvShows }) {
       <MovieRow title="Popular" movies={movies.popular} series={tvShows.popular} />
 
 
-         <NativeAd />
+         
 
  
 
 
       <MovieRow title="Trending" movies={movies.trending} series={tvShows.trending} />
 
-       <AdsterraBanner
-    adKey="b37c8ffb927736e7563e62d79974015f"
-    width={420}
-    height={60}
-/>
-       
+ 
       
       <MovieRow title="Top Rated" movies={movies.topRated} series={tvShows.topRated} />
     </div>
