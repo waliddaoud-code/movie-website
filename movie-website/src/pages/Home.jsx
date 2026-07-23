c961d3f0c9755a4fa9e181968503568412df5228
walid daoud <daoud.walid.2002@gmail.com>
2026-07-23 13:22:32 +0100

    popunder

--- movie-website/src/pages/Home.jsx
+++ movie-website/src/pages/Home.jsx
@@ -1,32 +1,42 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import "../css/Home.css";
 import MovieRow from "../components/MovieRow/MovieRow";
 import Poster from "../components/Poster";
 
-
-
 function Home({ movies, tvShows }) {
- 
-  
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/popunder.js"; // public/popunder.js
+    script.async = true;
 
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div className="home">
       <Poster movies={movies.trendingAll} />
 
-      <MovieRow title="Popular" movies={movies.popular} series={tvShows.popular} />
+      <MovieRow
+        title="Popular"
+        movies={movies.popular}
+        series={tvShows.popular}
+      />
 
+      <MovieRow
+        title="Trending"
+        movies={movies.trending}
+        series={tvShows.trending}
+      />
 
-         
-
- 
-
-
-      <MovieRow title="Trending" movies={movies.trending} series={tvShows.trending} />
-
- 
-      
-      <MovieRow title="Top Rated" movies={movies.topRated} series={tvShows.topRated} />
+      <MovieRow
+        title="Top Rated"
+        movies={movies.topRated}
+        series={tvShows.topRated}
+      />
     </div>
   );
 }
